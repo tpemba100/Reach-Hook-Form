1ce1d06005054caefe5ba6d24fb72c0d24551ef1
tpemba100 <tpemba.96@gmail.com>
2022-10-12 15:09:40 -0400

    changes and submission pop up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
-import "./index.css";
+// import "./index.css";
+
 
 function App() {
+  
+  const [state, setState] = useState(false);
+
+  const toggleButton = (event) => {
+    setState(current => !current);
+
+    event.preventDefault();
+
+    console.log("running")
+  }
+
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return; // 👈️ return early if first render
+    }
+
+    console.log('state: ', state);
+  }, [state]);
+
   return (
     <div class="form-container">
       <form class="register-form">
        
-        <div class="success-message">Success! Thank you for registering</div> */
+        {state && <div class="success-message">Success! Thank you for registering</div>} 
         <input
           id="first-name"
           class="form-field"
@@ -16,7 +40,7 @@ function App() {
           name="firstName"
         />
         
-        <span id="first-name-error">Please enter a first name</span> */
+       { !state && <span id="first-name-error">Please enter a first name</span>}
         <input
           id="last-name"
           class="form-field"
@@ -25,7 +49,7 @@ function App() {
           name="lastName"
         />
        
-        <span id="last-name-error">Please enter a last name</span> */
+        {!state && <span id="last-name-error">Please enter a last name</span>}
         <input
           id="email"
           class="form-field"
@@ -34,8 +58,8 @@ function App() {
           name="email"
         />
         
-         <span id="email-error">Please enter an email address</span> */
-        <button class="form-field" type="submit">
+         {!state && <span id="email-error">Please enter an email address</span>}        
+         <button class="form-field" type="submit" onClick={toggleButton}>
           Register
         </button>
       </form>
